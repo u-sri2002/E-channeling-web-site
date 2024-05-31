d7e4a5bbe2e3f7f187bc655c083610b5403bbb3e
u-sri2002 <usrimart@gmail.com>
2024-05-31 13:16:26 +0530

    add styles for form validation

--- js/registerAsDoc.js
+++ js/registerAsDoc.js
@@ -1,5 +1,4 @@
 const signupForm = document.getElementById("r-signup");
-
 const userName = document.getElementById("name");
 const email = document.getElementById("email");
 const mobile = document.getElementById("mobile");
@@ -7,107 +6,34 @@ const experience = document.getElementById("experience");
 const password = document.getElementById("password");
 const residence = document.getElementById("residence");
 
-signupForm.addEventListener('submit', function(submission) {
-
-    submission.preventDefault();
-
-    formInputsValidation();
-});
 
 
-function formInputsValidation(){
-    // get the values from inputs 
-    let UserNameValue = userName.value.trim();
-    let emailValue = email.value.trim();
-    let mobileValue = mobile.value.trim();
-    let experienceValue = experience.value.trim();
-    let residenceValue = residence.value.trim();
-    let passwordValue = password.value.trim();
-
-    // userName validation 
-    if(UserNameValue === ''){
-        //send error
-        sendErrorFor(userName, "User name can't be Null");      
-    }
-    else{
-        //send success
-        sendSuccessFor(userName);
-    }
-
-    // email validation 
-    if( emailValue === ''){
-        sendErrorFor(email, "Email is required");
-    }
-    else {
-        if(!isEmail(emailValue)) {
-        sendErrorFor(email, "Email is not in valid format ");
-      }
-        else{
-            sendSuccessFor(email);
-        }
-    }
-
-    // mobile validation 
-    if (mobileValue === ''){
-        sendErrorFor(mobile, "Mobile number is required ");
-    }
-    else if ( mobileValue.length != 10){
-        sendErrorFor(mobile, "Mobile number should have 10 numbers ");
-    }
-    else{
-        sendSuccessFor(mobile);
-    }
-
-    // year validation 
-    if (experienceValue === ''){
-        sendErrorFor(experience, "Experience duration is required");
-    }
-    else{
-        if( Number(experienceValue) < 0 ){
-            sendErrorFor(experience, "Please enter valid duration");
-        }
-        else if( Number(experienceValue) > 60 ){
-            sendErrorFor(experience, "Your are not eligible, We are very sorry..");
-        }
-        else{
-            sendSuccessFor(experience);
-        }
-    }
-
-    // residence validation 
-    if ( residenceValue === ''){
-        sendErrorFor(residence, "This field is required");
-    }
-    else{
-        sendSuccessFor(residence);
-    }
-
-    // password validation 
-    validatePassword()
-
-}
 function sendErrorFor(input, message){
     //send error message
     let smallTag = input.parentElement.querySelector("small");
     smallTag.innerHTML = message;
 
     //send styles to error
-    input.parentElement.className = "form-fields error"
+    input.parentElement.className = "form-fields error";
+
+    input.parentElement.querySelector("i").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4m.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
+    </svg>`;
 }
 
+//send styles to success
 function sendSuccessFor(input){
-
-    //send styles to error
-    input.parentElement.className = "form-fields success"
+    input.parentElement.className = "form-fields success hide-text"
 }
 
+// function to check correct email format
 function isEmail(email){
     const emailRegex =  /^(([^<>()\].,;:\s@"]+(\.[()\[\\.,;:\s@"]+)*)|(".+"))@(([0−9]1,3\.[0−9]1,3\.[0−9]1,3\.[0−9]1,3)|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
 }
 
-//passwords validation 
 
+//A function for passwords validation 
 function validatePassword() {
     const password = document.getElementById('password');
     const passwordValue = password.value;
@@ -134,3 +60,91 @@ function validatePassword() {
         sendSuccessFor(password);
     }
 }
+
+
+function formInputsValidation(){
+    // get the values from inputs 
+    let UserNameValue = userName.value.trim();
+    let emailValue = email.value.trim();
+    let mobileValue = mobile.value.trim();
+    let experienceValue = experience.value.trim();
+    let residenceValue = residence.value.trim();
+    let passwordValue = password.value.trim();
+
+
+    // userName validation 
+    if(UserNameValue === ''){
+        //send error
+        sendErrorFor(userName, "User name can't be Null");      
+    }
+    else{
+        //send success
+        sendSuccessFor(userName);
+    }
+
+
+    // email validation 
+    if( emailValue === ''){
+        sendErrorFor(email, "Email is required");
+    }
+    else {
+        if(!isEmail(emailValue)) {
+        sendErrorFor(email, "Email is not in valid format ");
+      }
+        else{
+            sendSuccessFor(email);
+        }
+    }
+
+
+    // mobile validation 
+    if (mobileValue === ''){
+        sendErrorFor(mobile, "Mobile number is required ");
+    }
+    else if ( mobileValue.length != 10){
+        sendErrorFor(mobile, "Mobile number should have 10 numbers ");
+    }
+    else{
+        sendSuccessFor(mobile);
+    }
+
+
+    // year validation 
+    if (experienceValue === ''){
+        sendErrorFor(experience, "Experience duration is required");
+    }
+    else{
+        if( Number(experienceValue) < 0 ){
+            sendErrorFor(experience, "Please enter valid duration");
+        }
+        else if( Number(experienceValue) > 60 ){
+            sendErrorFor(experience, "Your are not eligible, We are very sorry..");
+        }
+        else{
+            sendSuccessFor(experience);
+        }
+    }
+
+
+    // residence validation 
+    if ( residenceValue === ''){
+        sendErrorFor(residence, "This field is required");
+    }
+    else{
+        sendSuccessFor(residence);
+    }
+
+    // call function for password validation 
+    validatePassword();
+
+}
+
+
+// preventing default submissions
+signupForm.addEventListener('submit', function(submission) {
+
+    submission.preventDefault();
+
+    formInputsValidation();
+   
+});
