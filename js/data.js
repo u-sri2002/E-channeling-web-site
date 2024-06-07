7528300018a1a15f17bd5e58a009c375c101021c
pathummadhusanka <pathummadhusanka.dev@gmail.com>
2024-06-07 10:36:58 +0530

    Add specialists table v2

---
+++ js/data.js
@@ -0,0 +1,29 @@
+// Doctor list
+docList = [
+    {
+        "reg": 1100,
+        "name" : "John Doe",
+        "position": "Cardiologist",
+        "specialization": "Cardiology",
+        "availability" : true
+    },{
+        "reg": 1200,
+        "name" : "Mark Ben",
+        "position": "Cardiologist",
+        "specialization": "Cardiology",
+        "availability" : false
+    },{
+        "reg": 2100,
+        "name" : "Mike Thomson",
+        "position": "Radiologist",
+        "specialization": "Radiology",
+        "availability" : false
+    },{
+        "reg": 1300,
+        "name" : "Kevin Johnson",
+        "position": "Cardiologist",
+        "specialization": "Cardiology",
+        "availability" : true
+    }
+
+];
