a82e2d3f929856aad99a362d9893096f705fe2ea
pathummadhusanka <pathummadhusanka.dev@gmail.com>
2024-06-11 09:41:12 +0530

    Fix specialists & services card filling functions

--- js/main.js
+++ js/main.js
@@ -2,27 +2,30 @@
 function specialistsCardSet() {
     const speCardContainer = document.querySelector('.spe-card-container');
 
-    for(let i = 0; i < speData.length; i++) {
-        let speCardObj = speData[i];
+    if(speCardContainer) {
 
-        let speCardTitle = speCardObj.title;
-        let speCardDesc = speCardObj.description;
+        for(let i = 0; i < speData.length; i++) {
+            let speCardObj = speData[i];
 
-        let speCard = document.createElement('div');
-        speCard.classList.add('spe-card');
-        speCard.setAttribute('onclick', `redirectToPageId('specialists.html', 'doc-table-${speCardTitle.toLowerCase()}')`);
+            let speCardTitle = speCardObj.title;
+            let speCardDesc = speCardObj.description;
 
-        let speCardFormat = `
-                <img class="spe-card-img" src="./img/Cardiologist-vector.jpeg" alt="${speCardTitle}">
-                <h6 class="spe-card-title">${speCardTitle}</h6>
-                <p class="spe-card-desc">${speCardDesc}</p>
-                <img class="see-more-arrow" src="./img/see-more-arrow.svg" alt="see more">
-                `;
+            let speCard = document.createElement('div');
+            speCard.classList.add('spe-card');
+            speCard.setAttribute('onclick', `redirectToPageId('specialists.html', 'doc-table-${speCardTitle.toLowerCase()}')`);
 
-        speCard.innerHTML = speCardFormat;
+            let speCardFormat = `
+                    <img class="spe-card-img" src="./img/Cardiologist-vector.jpeg" alt="${speCardTitle}">
+                    <h6 class="spe-card-title">${speCardTitle}</h6>
+                    <p class="spe-card-desc">${speCardDesc}</p>
+                    <img class="see-more-arrow" src="./img/see-more-arrow.svg" alt="see more">
+                    `;
 
-        speCardContainer.appendChild(speCard);
-        
+            speCard.innerHTML = speCardFormat;
+
+            speCardContainer.appendChild(speCard);
+            
+        }
     }
 }
 
@@ -31,27 +34,30 @@ specialistsCardSet();
 // Fill services cards
 function servicesCardSet() {
     const serCardContainer = document.querySelector('.ser-card-container');
+    
+    if(serCardContainer) {
 
-    let j = 0;
-    while(j < serData.length) {
+        let j = 0;
+        while(j < serData.length) {
 
-        let serCardObj = serData[j];
+            let serCardObj = serData[j];
 
-        let serCardTitle = serCardObj.title;
-        let serCardImg = serCardObj.img;
+            let serCardTitle = serCardObj.title;
+            let serCardImg = serCardObj.img;
 
-        let serCard = document.createElement('div');
-        serCard.classList.add('ser-card');
+            let serCard = document.createElement('div');
+            serCard.classList.add('ser-card');
 
-        let speCardFormat = `
-        <img class="ser-card-img" src="./img/${serCardImg}-vector.jpeg" alt="${serCardTitle}">
-        <h6 class="ser-card-title">${serCardTitle}</h6>`;
+            let speCardFormat = `
+            <img class="ser-card-img" src="./img/${serCardImg}-vector.jpeg" alt="${serCardTitle}">
+            <h6 class="ser-card-title">${serCardTitle}</h6>`;
 
-        serCard.innerHTML = speCardFormat;
+            serCard.innerHTML = speCardFormat;
 
-        serCardContainer.appendChild(serCard);
+            serCardContainer.appendChild(serCard);
 
-        j++;
+            j++;
+        }
     }
 }
 
